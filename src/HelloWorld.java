f2bb4743dc298d764920e169b5c25023672aa304
Samuel Navarro <snavarroleiva@gmail.com>
2019-01-30 20:17:30 -0800

    Making conflict for Rafael :)

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,11 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-<<<<<<< HEAD
 		System.out.println("Hello Sam!");
-=======
-		System.out.println("Hello Fernando!");
->>>>>>> 309b034bef91fe19b8895802d35dc680fd7a0bbe
 
 	}
 
